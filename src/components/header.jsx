d6e777b231a5e3a1e9bf8655c1e2e6aa4686b3f1
ArijitPatra2906 <patraarijit440@gmail.com>
2024-08-19 23:37:18 +0530

    minor changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
     <>
       <nav className="py-4 flex justify-between items-center">
         <Link to="/">
-          <img src="/logo.png" className="h-14 w-40" alt="Hirrd Logo" />
+          <img src="/logo.png" className="h-14 w-40" alt="devcareerhub Logo" />
         </Link>
         <div className="flex gap-8">
           <SignedOut>
